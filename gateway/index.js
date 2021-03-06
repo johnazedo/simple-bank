c3b03db499f2268d0853ed10eb9623700642c8a5
JohnAzedo <jplimao12@gmail.com>
2021-03-06 10:57:39 -0300

    [gateway] fix: Change initial endpoit from localhost:8002

--- gateway/index.js
+++ gateway/index.js
@@ -1,15 +1,13 @@
 //index.js
 const httpProxy = require('express-http-proxy');
 const express = require('express');
-const app = express();
-var logger = require('morgan');
  
-app.use(logger('dev'));
+const app = express();
  
 function selectProxyHost(req) {
     if (req.path.startsWith('/admin'))
         return 'http://localhost:8001/';
-    else if (req.path.startsWith('/server'))
+    else if (req.path.startsWith('/todos'))
         return 'http://localhost:8002/';
 }
  
