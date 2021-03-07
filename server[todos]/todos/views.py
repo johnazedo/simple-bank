68066bb48534f79fc7db59a57e4288da2879a068
JohnAzedo <jplimao12@gmail.com>
2021-03-07 11:36:57 -0300

    [auth] feature: Create auth server

--- server[todos]/todos/views.py
+++ server[todos]/todos/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
-from .models import Todo
-from .serializers import TodoSerializer
+from todos.models import Todo
+from todos.serializers import TodoSerializer
 from rest_framework.permissions import AllowAny
 
 
