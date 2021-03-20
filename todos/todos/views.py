bcc13d479f6fd69aa23805ff89f774a027ead650
JohnAzedo <jplimao12@gmail.com>
2021-03-20 08:34:55 -0300

    [todos] feature: Create todo logic

--- todos/todos/views.py
+++ todos/todos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, UpdateAPIView, ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework.response import Response
 from todos.models import Todo
 from todos.serializers import TodoSerializer
@@ -12,7 +12,7 @@ class ListTodo(ListCreateAPIView):
     serializer_class = TodoSerializer
 
 
-class UpdateTodo(UpdateAPIView):
+class UpdateTodo(RetrieveUpdateDestroyAPIView):
     permission_classes = [AllowAny]
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
