ac1a9b34bed53ca9d93319e6a005cd017a40159e
João Pedro Limão <jplimao12@gmail.com>
2021-03-22 19:25:17 -0300

    [todos] feature: Create migrations for categories

--- todos/todos/admin.py
+++ todos/todos/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Todo
+from .models import Todo, Category
 
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     pass
--- todos/todos/models.py
+++ todos/todos/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Todo(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     text = models.CharField(max_length=512, null=False)
     done = models.BooleanField(default=False)
@@ -13,3 +14,16 @@ class Todo(models.Model):
     
     def __str__(self):
         return self.text
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, null=False)
+    color = models.CharField(max_length=6, null=False)
+    icon = models.CharField(max_length=128, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
