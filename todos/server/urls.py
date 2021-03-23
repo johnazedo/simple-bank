60cea656ae46cd25d066a466225336418abb80e8
João Pedro Limão <jplimao12@gmail.com>
2021-03-23 19:37:35 -0300

    [todos] architecture: Split categories from todos

--- todos/server/urls.py
+++ todos/server/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from todos.urls import categories
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todos/', include('todos.urls'))
+    path('todos/', include('todos.urls')),
+    path('categories/', include(categories))
 ]
--- todos/todos/urls.py
+++ todos/todos/urls.py
@@ -5,6 +5,11 @@ from .views import ListTodos, DetailTodo, ListCategories, DetailCategory
 urlpatterns = [
     path('', ListTodos.as_view(), name='list-create-todos'),
     path('<int:pk>', DetailTodo.as_view(), name='detail-todos'),
-    path('categories/', ListCategories.as_view(), name='list-create-category'),
-    path('category/<int:pk>', DetailCategory.as_view(), name='detail-category')
 ]
+
+categories = ([
+        path('', ListCategories.as_view(), name='list-create-category'),
+        path('<int:pk>', DetailCategory.as_view(), name='detail-category')
+    ],
+    'categories'
+)
