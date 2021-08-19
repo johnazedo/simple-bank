a39d523fd374b0bf7a8ae3f1647dda1ecdf39f38
João Pedro Limão <jplimao12@gmail.com>
2021-08-19 11:36:24 -0300

    [news] Create upload file function

--- api/news/controllers/news_controller.go
+++ api/news/controllers/news_controller.go
@@ -3,10 +3,13 @@ package controllers
 import (
 	"encoding/json"
 	"github.com/go-chi/chi"
+	"io"
 	"log"
 	"net/http"
 	"news/database"
 	"news/domain"
+	"os"
+	"strings"
 )
 
 type NewsController struct{}
@@ -19,6 +22,8 @@ func (nc *NewsController) FetchAll(w http.ResponseWriter, r *http.Request) {
 
 func (nc *NewsController) Create(w http.ResponseWriter, r *http.Request) {
 	var news domain.News;
+
+	uploadFile(r)
 	err := json.NewDecoder(r.Body).Decode(&news)
 
 	if err != nil {
@@ -28,6 +33,36 @@ func (nc *NewsController) Create(w http.ResponseWriter, r *http.Request) {
 	database.GetDB().Create(&news)
 	json.NewEncoder(w).Encode(&news)
 }
+
+func uploadFile(r *http.Request) (*os.File) {
+	err := r.ParseMultipartForm(23 << 20);
+	if err != nil {
+		log.Println("PARSE_ERROR: Cannot get file!")
+		return nil
+	}
+
+	image, header, err := r.FormFile("imageUrl");
+	defer image.Close();
+
+	if err != nil {
+		log.Println("KEY_ERROR: Cannot get file!")
+		return nil
+	}
+
+	name := strings.Split(header.Filename, ".");
+	log.Println("File name ", name[0]);
+
+	file, err := os.OpenFile("./downloaded", os.O_WRONLY|os.O_CREATE, 0666)
+	defer file.Close()
+
+	if err != nil {
+		log.Fatalln("OS_FILE_ERROR: Cannot open os file!")
+	}
+
+	io.Copy(file, image)
+	return file
+}
+
 
 func (nc *NewsController) Get(w http.ResponseWriter, r *http.Request) {
 	var news domain.News;
