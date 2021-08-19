7540d4c5e35c15a0b6f2f9965cbeed973ffb2de4
João Pedro Limão <jplimao12@gmail.com>
2021-08-19 11:35:42 -0300

    [news] Change ID to UUID

--- api/news/domain/news.go
+++ api/news/domain/news.go
@@ -1,8 +1,20 @@
 package domain
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type News struct {
-	ID uint `gorm:"primaryKey" json:"id"`
+	ID uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
 	Title string `json:"title"`
 	Url string `json:"url"`
 	Author string `json:"author"`
+	Image string `json:"imageUrl"`
+}
+
+
+func (news* News) BeforeCreate(*gorm.DB) error{
+	news.ID = uuid.New()
+	return nil
 }
