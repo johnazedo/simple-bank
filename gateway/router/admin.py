271934d4e49224588a598132760adaf1569d95f9
João Pedro Limão <jplimao12@gmail.com>
2021-10-01 17:02:38 -0300

    Restart project

--- gateway/router/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import API
-
-@admin.register(API)
-class APIAdmin(admin.ModelAdmin):
-    pass
--- gateway/router/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Router
-namespace='router'
-
-urlpatterns = [
-    path('', Router.as_view(), name='all')
-]
